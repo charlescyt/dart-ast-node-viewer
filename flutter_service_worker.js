6847f09ec62dc6bbddd2417ae3bb0106b7978df6
charlescyt <charlescyt@users.noreply.github.com>
2024-07-10 16:21:30 +0000

    deploy: cf724a62e66905ee70646eff62f9447647790207

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "412dfbd7fd5a6279b63cf7640a561807",
+"flutter_bootstrap.js": "35551b46f1776714a5decc57484d7f94",
 "version.json": "d981406f004b470ccdc46a4d6160ac6c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/NOTICES": "9c3bb58eebfc8e94c9361fe2a2c12c22",
