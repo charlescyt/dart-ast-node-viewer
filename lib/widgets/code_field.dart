dbe6865ea8f418e0892eee1b3d5fe16cd5af3078
Charles Tsang <charles.cy.tsang@gmail.com>
2025-03-29 01:08:23 +0800

    Add line and column indicator

--- lib/widgets/code_field.dart
+++ lib/widgets/code_field.dart
@@ -5,7 +5,7 @@ import 'package:re_highlight/styles/monokai-sublime.dart';
 
 import 'app_decorated_box.dart';
 
-class CodeField extends StatelessWidget {
+class CodeField extends StatefulWidget {
   const CodeField({
     super.key,
     this.readOnly = false,
@@ -20,37 +20,93 @@ class CodeField extends StatelessWidget {
   final ValueChanged<String>? onContentChanged;
 
   @override
+  State<CodeField> createState() => _CodeFieldState();
+}
+
+class _CodeFieldState extends State<CodeField> {
+  late CodeLineEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? CodeLineEditingController();
+  }
+
+  @override
+  void didUpdateWidget(covariant CodeField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.controller != oldWidget.controller) {
+      _controller = widget.controller ?? CodeLineEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AppDecoratedBox(
-      child: CodeEditor(
-        controller: controller,
-        readOnly: readOnly,
-        wordWrap: wordWrap,
-        padding: const EdgeInsets.all(8.0),
-        onChanged: (value) => onContentChanged?.call(value.codeLines.asString(TextLineBreak.lf)),
-        style: CodeEditorStyle(
-          fontSize: 14,
-          fontFamily: 'JetBrainsMono',
-          codeTheme: CodeHighlightTheme(
-            languages: {'dart': CodeHighlightThemeMode(mode: langDart)},
-            theme: monokaiSublimeTheme,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: CodeEditor(
+              controller: _controller,
+              readOnly: widget.readOnly,
+              wordWrap: widget.wordWrap,
+              padding: const EdgeInsets.all(8.0),
+              onChanged: (value) => widget.onContentChanged?.call(value.codeLines.asString(TextLineBreak.lf)),
+              style: CodeEditorStyle(
+                fontSize: 14.0,
+                fontFamily: 'JetBrainsMono',
+                codeTheme: CodeHighlightTheme(
+                  languages: {'dart': CodeHighlightThemeMode(mode: langDart)},
+                  theme: monokaiSublimeTheme,
+                ),
+              ),
+              indicatorBuilder: (context, editingController, chunkController, notifier) {
+                return Row(
+                  children: [
+                    DefaultCodeLineNumber(
+                      notifier: notifier,
+                      controller: editingController,
+                    ),
+                    DefaultCodeChunkIndicator(
+                      width: 20.0,
+                      notifier: notifier,
+                      controller: chunkController,
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
-        ),
-        indicatorBuilder: (context, editingController, chunkController, notifier) {
-          return Row(
-            children: [
-              DefaultCodeLineNumber(
-                notifier: notifier,
-                controller: editingController,
-              ),
-              DefaultCodeChunkIndicator(
-                width: 20,
-                notifier: notifier,
-                controller: chunkController,
-              ),
-            ],
-          );
-        },
+          const Divider(height: 1.0),
+          ValueListenableBuilder(
+            valueListenable: _controller,
+            builder: (context, value, child) {
+              final cursorPosition = value.selection.extent;
+              final line = cursorPosition.index;
+              final col = cursorPosition.offset;
+              return ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 20.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  child: Text(
+                    'Ln $line, Col $col',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
